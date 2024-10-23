0d7eb3ff561596123bef156362fbd5bcf383f8ff
DripPapi <Junweiseeto97@hotmail.com>
2024-10-23 16:18:03 +0800

    app routing changes

--- portfolio-website/src/App.tsx
+++ portfolio-website/src/App.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home"
 import Services from "@/scenes/services"
-import Stats from "@/scenes/stats"
-import Footer from "@/scenes/footer"
+import About from "@/scenes/about"
+import Contact from "@/scenes/contact"
 import { SelectedPage } from "@/shared/types";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -15,7 +16,7 @@ function App() {
     const handleScroll = () => {
       if (window.scrollY === 0) {
         setIsTopOfPage(true);
-        setSelectedPage(SelectedPage.Home);
+        // setSelectedPage(SelectedPage.Home);
       }
       if (window.scrollY !== 0) setIsTopOfPage(false);
     };
@@ -25,16 +26,18 @@ function App() {
 
   return (
     <>
-      <div className="app bg-primary-100">
+      <div className="app bg-primary-100 min-h-screen flex flex-col">
       <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-      <Home setSelectedPage={setSelectedPage} />
-      <Stats/>
-      <Services setSelectedPage={setSelectedPage}/>
-      <Footer />
+      <Routes>
+        <Route path="/home" element={<Home setSelectedPage={setSelectedPage} />} />
+        <Route path="/about" element={<About setSelectedPage={setSelectedPage}  />} />
+        <Route path="/services" element={<Services setSelectedPage={setSelectedPage}/>} />
+        <Route path="/contact" element={<Contact setSelectedPage={setSelectedPage}/>} />
+      </Routes>
       </div> 
     </>
   );
