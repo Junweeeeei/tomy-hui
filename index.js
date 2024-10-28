dd703fddf36b481f15685412ca585c35158c1b30
DripPapi <Junweiseeto97@hotmail.com>
2024-10-28 18:59:55 +0800

    Express code used for google cloud run web hosting

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.static('web-app/dist'));
+const port = process.env.PORT || 8800;
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
