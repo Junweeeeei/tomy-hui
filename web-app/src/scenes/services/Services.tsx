6bd316fe3316d39a2f5b1972ece9479e10efa879
DripPapi <Junweiseeto97@hotmail.com>
2024-10-28 15:10:48 +0800

    added route into service components for clicking

--- web-app/src/scenes/services/Services.tsx
+++ web-app/src/scenes/services/Services.tsx
@@ -11,17 +11,18 @@ type Props = {
   description: string;
   img: string;
   setSelectedPage: (value: SelectedPage) => void;
+  route: string;
 };
 
-const Services = ({ title, description, img, setSelectedPage }: Props) => {
+const Services = ({ title, img, setSelectedPage, route }: Props) => {
   return (
     <motion.div
       variants={childVariant}
-      className="elative group overflow-hidden transition-shadow duration-300 hover:shadow-2xl rounded-md"
+      className="relative group overflow-hidden transition-shadow duration-300 hover:shadow-2xl rounded-md"
       onViewportEnter={() => setSelectedPage(SelectedPage.Services)}
     >
       <a 
-        href="#" 
+        href={route} 
         key={title} 
         className="relative"
       >
@@ -35,9 +36,8 @@ const Services = ({ title, description, img, setSelectedPage }: Props) => {
         {/* Solid Transparent Blue Section */}
         <div className="absolute bottom-0 left-0 right-0 bg-blue-600/40 h-1/3 flex items-end p-6">
             <div className="text-white text-2xl font-bold text-center flex items-center justify-center w-full h-full"> {/* Centering text */}
-              {description}
+              {title}
             </div>
-            {/* <p className="text-white text-sm">{description}</p> */}
         </div>
       </a>
     </motion.div>
