b022c40d6a5d31aebaee0bd533f8f9cfbadc44ad
DripPapi <Junweiseeto97@hotmail.com>
2024-11-21 18:17:27 +0800

    standardized text and header sizes

--- web-app/src/scenes/stats/index.tsx
+++ web-app/src/scenes/stats/index.tsx
@@ -1,5 +1,8 @@
 import Counter from "@/shared/Counter"
 
+const HEADER_SIZE="md:text-xl lg:text-2xl"
+const TEXT_SIZE="md:text-l lg:text-xl"
+
 const stats_des = `With over 25 years of expertise, Tomy Hui Electrical Engineering Pte Ltd has established a 
                   strong reputation in Singapore’s electrical engineering industry through dedication, quality, and continuous growth. 
                   Our services span the maintenance, installation, and enhancement of electrical systems across diverse sectors, 
@@ -11,18 +14,18 @@ const Stats = () => {
     <section className="bg-gray-20 py-16">
       <div className="justify-content mx-auto w-1/2 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
-          <p className="text-xl font-extrabold text-red-900 ">
+          <p className={`${TEXT_SIZE} font-extrabold text-red-900`}>
             About us
           </p>
-          <p className="my-5 text-2xl font-bold text-blue-900">
+          <p className={`${HEADER_SIZE} my-5 font-bold text-blue-900`}>
             Meet Tomy Hui Engineering
           </p>
-          <p className="text-xl text-black">
+          <p className={`${TEXT_SIZE} text-black`}>
             {stats_des}
           </p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
-          <div className="basis-1/8 md:mt-0 font-bold text-blue-900 text-2xl">
+          <div className={`${HEADER_SIZE} basis-1/8 md:mt-0 font-bold text-blue-900`}>
             Years Of Experience
           </div>
           {/* Countup timer */}
@@ -31,7 +34,7 @@ const Stats = () => {
           </div>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
-          <div className="basis-1/8 md:mt-0 font-bold text-blue-900 text-2xl">
+          <div className={`${HEADER_SIZE} basis-1/8 md:mt-0 font-bold text-blue-900`}>
             Projects Completed
           </div>
           {/* Countup timer */}
