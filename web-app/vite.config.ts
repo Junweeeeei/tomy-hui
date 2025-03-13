bbff64b59110ca42a547f286558d928f04e4db0e
DripPapi <Junweiseeto97@hotmail.com>
2025-03-13 14:42:08 +0800

    Fix routing issue for Render Hosting

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -5,8 +5,8 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
-  // base: "/tomy-hui/",
 });
